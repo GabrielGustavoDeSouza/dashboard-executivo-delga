96a6eb2bbba02ab082c80bee0130e16ad2dc5d68
GabrielGustavoDeSouza <gabriel.souza.gustavo@gmail.com>
2026-06-24 11:25:00 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard Executivo Delga",
+    layout="wide"
+)
+
+st.title("Dashboard Executivo — Grupo Delga")
+st.write("Estrutura inicial criada com sucesso.")
